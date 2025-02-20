728e2c6f989e5956db19c7fe3a8c1079adf94cc1
Juanchis <juabelosp@gmail.com>
2025-02-20 16:29:10 -0500

    implementacion del primer webhook

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
+const bodyParser = require('body-parser');
 const app = (0, express_1.default)();
-const port = 3000;
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('prueba amaris');
+    res.send('HELOUU...');
+});
+app.get('/webhook', (req, res) => {
+    const VERIFY_TOKEN = process.env.WEBHOOK_TOKEN;
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log('Webhook verificado');
+        res.status(200).send(challenge);
+    }
+    else {
+        res.sendStatus(403);
+    }
 });
 app.listen(port, () => {
-    console.log("conectado y cooperando");
+    console.log("activo");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
-import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import express from 'express';
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
-app.get('/',(req:Request, res: Response) =>{
-  res.send('prueba amaris');
+app.get('/',(req, res) =>{
+  res.send('HELOUU...'); 
 });
 
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = process.env.WEBHOOK_TOKEN; 
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('Webhook verificado');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+
 app.listen(port, ()=>{
-  console.log("conectado y cooperando");
+  console.log("activo");
 });
 
